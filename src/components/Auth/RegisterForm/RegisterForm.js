ca4f9a71f9a2f6e4546ce001cbc18a075e8af80f
Daniel Lopez <d.lopez.1740@gmail.com>
2022-04-30 21:04:02 -0400

    ready register

--- src/components/Auth/RegisterForm/RegisterForm.js
+++ src/components/Auth/RegisterForm/RegisterForm.js
@@ -1,7 +1,55 @@
-export default function RegisterForm() {
+import { Button, Icon, Form, Input } from 'semantic-ui-react';
+import { auth } from '../../../utils/Firebase';
+
+import './RegisterForm.scss';
+
+export default function RegisterForm({ setSelectedForm }) {
+  const onSubmit = () => {
+    console.log('Formulario enviado');
+  };
+
   return (
-    <span>
-      RegisterForm...
-    </span>
+    <div className="register-from">
+      <h1>Empieza a escuchar con una cuenta de Musify gratis.</h1>
+      <Form onSubmit={onSubmit}>
+        <Form.Field>
+          <Input
+            type="text"
+            name="email"
+            placeholder="Correo electrónico"
+            icon="mail outline"
+            // onChange={}
+            // error={}
+          />
+        </Form.Field>
+        <Form.Field>
+          <Input
+            type="password"
+            name="password"
+            placeholder="Contraseña"
+            icon="eye"
+            // onChange={}
+            // error={}
+          />
+        </Form.Field>
+        <Form.Field>
+          <Input
+            type="text"
+            name="username"
+            placeholder="¿Como deberiamos llamarte?"
+            icon="user circle outline"
+            // onChange={}
+            // error={}
+          />
+        </Form.Field>
+        <Button type="submit">Continuar</Button>
+      </Form>
+      <div className="register-from__options">
+        <p onClick={() => setSelectedForm(null)}>Volver</p>
+        <p>
+          ¿Ya tienes Musicfy? <span onClick={() => setSelectedForm('login')}>Iniciar sesión</span>
+        </p>
+      </div>
+    </div>
   );
 }
