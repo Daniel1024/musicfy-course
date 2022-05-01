87bae7b8dc39bc78dca96e7b2aff31ffb0c11fb5
Daniel Lopez <d.lopez.1740@gmail.com>
2022-04-30 20:36:00 -0400

    ready AuthOptions

--- src/components/Auth/AuthOptions/AuthOptions.js
+++ src/components/Auth/AuthOptions/AuthOptions.js
@@ -1,7 +1,14 @@
-export default function AuthOptions() {
+import { Button } from 'semantic-ui-react';
+import './AuthOptions.scss';
+
+export default function AuthOptions({ setSelectedForm }) {
   return (
     <span>
-      AuthOptions...
+      <div className="auth-options">
+        <h2>Millones de canciones, gratis en Musicfy</h2>
+        <Button className="register" onClick={() => setSelectedForm('register')}>Registarte gratis</Button>
+        <Button className="login" onClick={() => setSelectedForm('login')}>Iniciar sessión</Button>
+      </div>
     </span>
   );
 }
